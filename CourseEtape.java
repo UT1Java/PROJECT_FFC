7f31ac20390e035324cec0f45adb619e09556e20
wwweichennn <116090231+wwweichennn@users.noreply.github.com>
2022-10-18 17:18:56 +0200

    Update CourseEtape.java

--- CourseEtape.java
+++ CourseEtape.java
@@ -2,12 +2,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ffc;
+package ffc.model;
+
+import java.util.HashMap;
 
 /**
  *
  * @author sunweichen
  */
-public class CourseEtape {
+public class CourseEtape extends Course{
+    
+    public HashMap<OrdreEtape,Etape> tableEtape;
+    
+    public double distanceCE;
+    
+    public CourseEtape(String nomCourse){
+        super(nomCourse);
+    }
+    
+    public void ajouterEtape(int numEtape,Etape e) {
+        OrdreEtape o=new OrdreEtape(numEtape);
+	tableEtape.put(o, e);
+    }
+    
+     public void supprimerEtape(int numEtape) {
+        OrdreEtape o=new OrdreEtape(numEtape);
+	tableEtape.remove(o);
+    }
     
 }
