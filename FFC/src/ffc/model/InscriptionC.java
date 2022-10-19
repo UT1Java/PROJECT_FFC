dc012d6f24f623db27ba55daa29282f459406453
Genevieve31200 <116088291+Genevieve31200@users.noreply.github.com>
2022-10-19 11:44:23 +0200

    Update InscriptionC.java

--- FFC/src/ffc/model/InscriptionC.java
+++ FFC/src/ffc/model/InscriptionC.java
@@ -9,5 +9,84 @@ package ffc.model;
  * @author sunweichen
  */
 public class InscriptionC {
+    //Constructeur de la classe "InscriptionC"
+    private int numIncriptionC;
+    private String etatInscriptionC;
+    private String dateInscription;
+    private String etatCoureur;
     
+    //Construteur de la classe "InscriptionC"
+    public InscriptionC(){
+        
+    }
+    
+    //Génération des getteurs et des setteurs
+
+    /**
+     * @return the numIncriptionC
+     */
+    public int getNumIncriptionC() {
+        return numIncriptionC;
+    }
+
+    /**
+     * @return the etatInscriptionC
+     */
+    public String getEtatInscriptionC() {
+        return etatInscriptionC;
+    }
+
+    /**
+     * @param etatInscriptionC the etatInscriptionC to set
+     */
+    public void setEtatInscriptionC(String etatInscriptionC) {
+        this.etatInscriptionC = etatInscriptionC;
+    }
+
+    /**
+     * @return the dateInscription
+     */
+    public String getDateInscription() {
+        return dateInscription;
+    }
+
+    /**
+     * @return the etatCoureur
+     */
+    public String getEtatCoureur() {
+        return etatCoureur;
+    }
+
+    /**
+     * @param etatCoureur the etatCoureur to set
+     */
+    public void setEtatCoureur(String etatCoureur) {
+        this.etatCoureur = etatCoureur;
+    }
+    
+    //Méthodes de la classe "InscriptionC"
+    public void InscriptionCoureur(){
+        
+    }
+    
+    public Coureur obtrenirCoureur(){
+        
+        return null;
+    }
+    
+    public void ajouterCoureur(){
+        
+    }
+    
+    public void supprimerInscriptionCoureur(){
+        
+    }
+    
+    public void modifEtatInscriptionC(){
+        
+    }
+    
+    public void modifEtatCoureur(){
+        
+    }
 }
