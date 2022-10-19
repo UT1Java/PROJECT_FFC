a42a4d3a3918196113bc02528cf032492a7fc338
Genevieve31200 <116088291+Genevieve31200@users.noreply.github.com>
2022-10-19 23:04:48 +0200

    Update FFC.java

--- FFC/src/ffc/model/FFC.java
+++ FFC/src/ffc/model/FFC.java
@@ -53,6 +53,32 @@ public class FFC {
      System.out.println(cs1.getDistance());
      System.out.println(ce1.getDistance());
     
+        
+     //Test de la classe Equipe / Coureur et les différentes inscriptions
+        
+    //Création des coureurs
+     Coureur coureur1 = new Coureur(1, "Laffont", "Jovin", "06/07/2000", "négatif", "AB");
+     Coureur coureur2 = new Coureur(2, "Dupont", "Elina", "04/09/2001", "négatif", "A");
+     Coureur coureur3 = new Coureur(3, "lafontaine", "valsin", "08/03/1999", "positif", "A");
+     Coureur coureur4 = new Coureur(4, "Xavier", "victor", "05/07/2002", "négatif", "AB");
+     Coureur coureur5 = new Coureur(5, "Vacquer", "alex", "08/10/2000", "négatif", "B");
+     
+     //Création des équipes
+     Equipe equipe1 = new Equipe(1, "B&B", "Française", "LIFFOR", "0398746490", "eghegge@hjesh.com");
+     
+     //ajouter un coureur a une équipe
+     equipe1.ajouterCoureurs(coureur1);
+     equipe1.ajouterCoureurs(coureur2);
+     equipe1.ajouterCoureurs(coureur3);
+     equipe1.ajouterCoureurs(coureur4);
+     equipe1.ajouterCoureurs(coureur5);
+     //Visualiser les coureurs d'une équipe
+     equipe1.obtenirTousLesCoureurs();
+     
+     //Création d'un véhicule d'assistance
+     VehiculesAssistances vehicule1 = new VehiculesAssistances(123984, "TOYOTA", "CV");
+     VehiculesAssistances vehicule2 = new VehiculesAssistances(123479, "CITROEN", "TV");
+     VehiculesAssistances vehicule3 = new VehiculesAssistances(149384, "MERCEDES", "IV");
     }
     
 }
