1b8e692f051a92be5193c711309ecb3c40936670
Genevieve31200 <116088291+Genevieve31200@users.noreply.github.com>
2022-10-19 11:43:30 +0200

    Update and rename Vehicule.java to VehiculesAssistances.java

---
+++ FFC/src/ffc/model/VehiculesAssistances.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ffc.model;
+
+/**
+ *
+ * @author Geneviève
+ */
+public class VehiculesAssistances {
+    //Attributs de la classe "VehiculesAssistances"
+    private int numImmatriculation;
+    private String marque;
+    private String typeV;
+    
+    //Constructeur de la classe "VehiculesAssistances"
+    public VehiculesAssistances(){
+        
+    }
+    
+    //Génération des getteurs et des setteurs
+    /**
+     * @return the numImmatriculation
+     */
+    public int getNumImmatriculation() {
+        return numImmatriculation;
+    }
+
+    /**
+     * @return the marque
+     */
+    public String getMarque() {
+        return marque;
+    }
+
+    /**
+     * @return the typeV
+     */
+    public String getTypeV() {
+        return typeV;
+    }
+    
+    //Méthodes de la classe "VehiculesAssistances"
+    public void ajouterVehicule(){
+        
+    }
+    
+    public void supprimerVehicule(){
+        
+    }
+    
+    public void modifierVehicule(){
+        
+    }
+}
