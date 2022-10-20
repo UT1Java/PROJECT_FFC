11ab44463315746f70f9354327aab17f36155f93
wwweichennn <116090231+wwweichennn@users.noreply.github.com>
2022-10-20 12:21:58 +0200

    Update Col.java

--- FFC/src/ffc/model/Col.java
+++ FFC/src/ffc/model/Col.java
@@ -4,10 +4,42 @@
  */
 package ffc.model;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+
 /**
  *
  * @author sunweichen
  */
 public class Col {
     
+    public int numCol;
+      private HashMap<Coureur,PointsCol> TableCol=new HashMap<>();
+      
+      public Col(int col){
+        this.numCol=col;
+        this.TableCol=new HashMap<Coureur,PointsCol>();
+        
+    }
+      public int getnumSprint(){
+      return this.numCol;
+      }
+              
+     public void ajouterTcol(int ordre,Coureur c){
+        switch (ordre) {
+            case 1:
+                this.TableCol.put(c,new PointsCol(ordre,5));
+                break;
+            case 2:
+                this.TableCol.put(c,new PointsCol(ordre,3));
+                break;
+            case 3:
+                this.TableCol.put(c,new PointsCol(ordre,2));
+                break;
+            default:
+                this.TableCol.put(c,new PointsCol(ordre,1));
+                break;
+        }
+    }
 }
+
