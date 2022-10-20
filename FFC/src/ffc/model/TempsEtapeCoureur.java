d3268b1d5413d66211e952eb40e379d6de7ed98c
wwweichennn <116090231+wwweichennn@users.noreply.github.com>
2022-10-20 12:24:04 +0200

    Update TempsEtapeCoureur.java

--- FFC/src/ffc/model/TempsEtapeCoureur.java
+++ FFC/src/ffc/model/TempsEtapeCoureur.java
@@ -9,5 +9,21 @@ package ffc.model;
  * @author sunweichen
  */
 public class TempsEtapeCoureur {
+    public float tempsE;
+    public String EtatTempsEtape="inverifie";
     
+    public TempsEtapeCoureur(float t){
+        this.tempsE=t;
+    }
+    public float getTempsEtape(){
+        return this.tempsE;
+    }
+    
+    public void validerTemps(){
+        this.EtatTempsEtape="Valide";    
+    }
+    
+     public void invaliderTemps(){
+        this.EtatTempsEtape="Invalide";    
+    }
 }
